dc239db3ac69a52f249583db95c7a7633300204d
Kuesung Park <gueit214@naver.com>
2023-04-07 22:45:18 +0900

    refactor: change component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Test from "./pages/begin";
+import Begin from "./pages/begin";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Test />} />
-          <Route path="/begin" element={<Test />} />
+          <Route path="/" element={<Begin />} />
+          <Route path="/begin" element={<Begin />} />
         </Routes>
       </div>
     </BrowserRouter>
